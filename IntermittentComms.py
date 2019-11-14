Fixed issue in steering function which caused the time cost computation to be wrong, did forget to round to integer after computing vnew. Corrected rewiring function and added updateSuccessor nodes function which is also finished

IntermittentComms.py
@@ -10,6 +10,34 @@ import numpy as np
 from operator import itemgetter
 import networkx as nx
 
+def updateSuccessorNodes(robot, startNode, uMax):
+    """Update successor node weight and times as the rewiring changed the graph structure"""
+    # Input arguments
+    # robot = which robot graph that we are analyzing
+    # startNode = which node got changed
+    # uMax = maximum velocity of robot for cost computation
+    
+    allSuccessors = nx.dfs_successors(robot.graph,startNode)
+    print('updateSuccesorNodes')
+    
+    for key, value in allSuccessors.items():
+        for v in value:
+            
+            startTime = robot.graph.nodes[key]['t']
+            startPos = robot.graph.nodes[key]['pos']
+            succPos = robot.graph.nodes[v]['pos']
+            succTotalCost, succEdgeCost = cost(startTime,startPos,succPos,uMax)
+            
+            robot.graph[key][v]['weight'] = succEdgeCost
+            robot.graph.nodes[v]['t'] = succTotalCost
+            
+            print(key, '->', v)
+            print(robot.graph.nodes[v]['t'])
+            print(succTotalCost)
+            print(succEdgeCost)
+            print(robot.graph[key][v])
+            print(robot.graph.nodes[v])        
+
 def rewireGraph(robot, setVnear, uMax):
     # TODO: Check if vnew is in goal set
     # TODO: Find out what this min t = max t means
@@ -32,12 +60,16 @@ def rewireGraph(robot, setVnear, uMax):
         nearTotalCost, nearEdgeCost = cost(timeVnew,posVnew,vnearPos,uMax) 
 
         if nearTotalCost < vnearTime:
-            edge = robot.graph.edges(setVnear[n])
-            robot.graph.remove_edge(list(edge)[0][0],list(edge)[0][1])
+            predecessor = list(robot.graph.pred[setVnear[n]])[-1]
+            print(robot.ID)
+            print(predecessor,setVnear[n])
+            print(robot.vnewIdx,setVnear[n])
+            robot.graph.remove_edge(predecessor,setVnear[n])
             robot.graph.add_edge(robot.vnewIdx,setVnear[n], weight = nearEdgeCost)
             robot.graph.nodes[setVnear[n]]['t'] = nearTotalCost
-
-
+            
+            updateSuccessorNodes(robot, setVnear[n], uMax)
+            
 def cost(nearTime, nearPos, newPos, uMax):
     # TODO: add information gain cost
 
@@ -76,8 +108,7 @@ def extendGraph(robot, setVnear, uMax):
 
         if nearTotalCost < vminCost:
             robot.nearestNodeIdx = setVnear[n]
-            vminCost = nearTotalCost            
-    
+            vminCost = nearTotalCost     
     
     robot.vnewCost = nearEdgeCost
     robot.totalTime = vminCost
@@ -153,9 +184,11 @@ def steer(robots, team, teams, uMax, epsilon):
         deltaTcost = travelTime - (nearestTime - min(minTimes))
         
         if deltaTcost > 0:
-            vnew = nearestNode + uMax*deltaTcost*dist/normDist
+            
+            vnew = np.around(nearestNode + uMax*deltaTcost*dist/normDist)
             distVnew = np.sqrt(np.sum((nearestNode - vnew)**2))
             travelTimeVnew = distVnew/uMax
+
             
         else: # TODO: check again if this is correct
 #            print('Time delay too big')
@@ -213,31 +246,7 @@ def sampleVrand(discretization, rangeSamples, distribution = 'uniform'):
         if 0 <= vrand[0] <= discretization[0] and 0 <= vrand[1] <= discretization[1]:
             inBoundary = True
   
-    return vrand
-
-#TODO double check to make sure this works
-def findNearestAbove(myArray, target):
-    diff = myArray - target
-    mask = np.ma.lessEqual(diff, 0)
-    # We need to mask the negative differences and zero
-    # since we are looking for values above
-    if np.all(mask):
-        return None # returns None if target is greater than any value
-    maskedDiff = np.ma.maskedArray(diff, mask)
-    return maskedDiff.argmin()
-
-
-# TODO: double-check to make sure this works
-def findNearestBelow(myArray, target):
-    diff = myArray - target
-    mask = np.ma.greaterEqual(diff, 0)
-    # We need to mask the negative differences and zero
-    # since we are looking for values above
-    if np.all(mask):
-        return None # returns None if target is greater than any value
-    maskedDiff = np.ma.maskedArray(diff, mask)
-    return maskedDiff.argmax()
-
+    return np.around(vrand)
 
 
 class Robot:
@@ -264,8 +273,8 @@ class Robot:
 
         
         # Graph variables
-        self.graph = nx.Graph()
-        self.totalGraph = nx.Graph()
+        self.graph = nx.DiGraph()
+        self.totalGraph = nx.DiGraph()
         self.nodeCounter = 0
         self.nearestNodeIdx = 0
         self.vrand = np.array([0, 0])
@@ -278,7 +287,7 @@ class Robot:
         self.totalGraph = nx.compose(self.totalGraph,self.graph)
     
     def initializeGraph(self):
-        self.graph = nx.Graph()
+        self.graph = nx.DiGraph()
     
     def addNode(self, firstTime = False):
         """Add new node with pos and total time attributes and edge with edge travel time cost to graph based on self variables"""

TestIntermittent.py
@@ -8,7 +8,7 @@ Created on Mon Nov  4 10:48:29 2019
 
 import numpy as np
 from IntermittentComms import Schedule, Robot, sampleVrand, measurement, findNearestNode, steer, buildSetVnear, extendGraph, rewireGraph
-from Visualization import plotMatrix, plotMeetingGraphs
+from Visualization import plotMatrix, plotMeetingGraphs, clearPlots
 
 def main():
     """main test loop"""
@@ -63,13 +63,6 @@ def main():
     #robot test
     robots = testRobot(numRobots, teams, schedule)
     
-    #sample test
-#    rangeSamples = DISCRETIZATION #only for uniform
-#    vrand = sampleTest(rangeSamples,distribution='uniform')
-    
-#    rangeSamples = [[300,300],[10,10]] #only gaussian
-#    vrand = sampleTest(rangeSamples,distribution='gaussian')
-    
     # create the initial plans for all periods
     initialTime = 0
     for r in range(0,numRobots):
@@ -81,29 +74,17 @@ def main():
         for r in range(0,numRobots):
             robots[r].composeGraphs()
     
-    plotMeetingGraphs(robots, [0, 1])
-    plotMeetingGraphs(robots, [1, 2])
-    plotMeetingGraphs(robots, [2, 3])
-    plotMeetingGraphs(robots, [3, 0])
-    
-    
-#    print('Graph robot 0')        
-#    print(robots[0].totalGraph.nodes.data())
-#    
-#    print('Graph robot 1')        
-#    print(robots[1].totalGraph.nodes.data())
-#
-#    print('Graph robot 2')        
-#    print(robots[2].totalGraph.nodes.data())
-#    
-#    print('Graph robot 3')        
-#    print(robots[3].totalGraph.nodes.data())
-    
-    print('Times at node 20 and 40')
-    print('robot 0: 20: t = ', robots[0].totalGraph.nodes[20]['t'], '    40: t = ', robots[0].totalGraph.nodes[40]['t'])
-    print('robot 1: 20: t = ', robots[1].totalGraph.nodes[20]['t'], '    40: t = ', robots[1].totalGraph.nodes[40]['t'])
-    print('robot 2: 20: t = ', robots[2].totalGraph.nodes[20]['t'], '    40: t = ', robots[2].totalGraph.nodes[40]['t'])
-    print('robot 3: 20: t = ', robots[3].totalGraph.nodes[20]['t'], '    40: t = ', robots[3].totalGraph.nodes[40]['t'])
+    if DEBUG:
+        plotMeetingGraphs(robots, [0, 1])
+        plotMeetingGraphs(robots, [1, 2])
+        plotMeetingGraphs(robots, [2, 3])
+        plotMeetingGraphs(robots, [3, 0])
+        
+        print('Times at node 20 and 40')
+        print('robot 0: %d: t = ' %TOTALSAMPLES, robots[0].totalGraph.nodes[TOTALSAMPLES]['t'], '    %d: t = ' %(TOTALSAMPLES*2), robots[0].totalGraph.nodes[TOTALSAMPLES*2]['t'])
+        print('robot 1: %d: t = ' %TOTALSAMPLES, robots[1].totalGraph.nodes[TOTALSAMPLES]['t'], '    %d: t = ' %(TOTALSAMPLES*2), robots[1].totalGraph.nodes[TOTALSAMPLES*2]['t'])
+        print('robot 2: %d: t = ' %TOTALSAMPLES, robots[2].totalGraph.nodes[TOTALSAMPLES]['t'], '    %d: t = ' %(TOTALSAMPLES*2), robots[2].totalGraph.nodes[TOTALSAMPLES*2]['t'])
+        print('robot 3: %d: t = ' %TOTALSAMPLES, robots[3].totalGraph.nodes[TOTALSAMPLES]['t'], '    %d: t = ' %(TOTALSAMPLES*2), robots[3].totalGraph.nodes[TOTALSAMPLES*2]['t'])
 
 #    dataSensorMeasurements, totalMap = update(currentTime, robots, numRobots, locations)
 
@@ -163,17 +144,7 @@ def createInitialPaths(schedule, teams, robots, numRobots, period):
                 rewireGraph(robots[np.int(r-1)], setVnear, UMAX)
             
         teamsDone[np.int(team)] = True     
-        
-    paths = []
     
-#    if DEBUG:
-#        print('Graph robot 0')        
-#        print(robots[0].graph.nodes.data())
-#        
-#        print('Graph robot 1')        
-#        print(robots[1].graph.nodes.data())
-    
-    return paths
 
 def randomStartingPositions(numRobots):
     """Ensures that the starting position are exclusive within communication radius"""
@@ -342,23 +313,25 @@ if __name__ == "__main__":
     """Entry in Test Program"""
     
     """Setup"""
-    np.random.seed(1994)
+    np.random.seed(19)
+    
+    clearPlots()
     
     CASE = 3 #case corresponds to which robot structure to use (1 = 8 robots, 8 teams, 2 = 8 robots, 5 teams, 3 = 2 robots 2 teams)
     DEBUG = True #debug to true shows prints
     COMMRANGE = 3 # TODO: communication range for robots
     
     DISCRETIZATION = np.array([600, 600]) #grid space
-    RANDOMSAMPLESMAX = 10 #how many random samples before trying to converge for communication
-    TOTALSAMPLES = 20 #how many samples in total
+    RANDOMSAMPLESMAX = 35 #how many random samples before trying to converge for communication
+    TOTALSAMPLES = 70 #how many samples in total
 
     SENSORPERIOD = 20 #time between sensor measurement or between updates of data
     EIGENVECPERIOD = 40 #time between POD calculations
     
     TOTALTIME = 1000 #total execution time of program
     
-    UMAX = 10 # TODO: Max velocity, 30 pixel/second
-    EPSILON = 50 # TODO: Maximum step size of robots
+    UMAX = 25 # TODO: Max velocity, 30 pixel/second
+    EPSILON = 50. # TODO: Maximum step size of robots
     GAMMARRT = 100 # TODO: constant for rrt* algorithm, can it be calculated?
     
     main()