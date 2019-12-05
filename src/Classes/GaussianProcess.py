777f8e12844939822fee96586e4631d0d6dd5b3e
HannesRovina <hannes1_rovina@hotmail.com>
2019-12-05 10:53:30 -0500

    working demo with pyGPS, but I am switching now to GPy of GPflow. Probably GPy

--- src/Classes/GaussianProcess.py
+++ src/Classes/GaussianProcess.py
@@ -22,60 +22,34 @@ class GaussianProcess:
         y = robot.mapping
         self.model.getPosterior(x, y)
         
-    def demo1(self):
-#        demoData = np.load('Data/regression_data.npz')
-#        x = demoData['x']      # training data
-#        y = demoData['y']      # training target
-#        z = demoData['xstar']  # test data
-        
-        x = np.array([[4,2],[2,5],[5,5],[8,8]])
-        y = np.array([5,2,4,3])
-        z = np.array([[6,6],[7,3]])
-        model = pyGPs.GPR()      # specify model (GP regression)
-        model.getPosterior(x, y) # fit default model (mean zero & rbf kernel) with data
-        model.optimize(x, y)     # optimize hyperparamters (default optimizer: single run minimize)
-        model.predict(z)         # predict test cases
-#        model.plot()             # and plot result
-        
-
-        ys22 = np.reshape(model.ys2,(model.ys2.shape[0],))
-        X, Y = np.mgrid[0:10:1, 0:10:1]
-        
-        pos = np.empty(X.shape + (2,))
-        pos[:, :, 0] = X; pos[:, :, 1] = Y
-        plt.figure()
-        rv = multivariate_normal(model.ym.reshape(2), ys22).pdf(pos)
-
-
-        plt.contour(X, Y, rv)
-        plt.colorbar()
-        
     def demo(self):
         mean = [0, 0]
         cov = [[1, 0], [0, 100]]  # diagonal covariance
-        
-        x, y = np.random.multivariate_normal(mean, cov, 10).T
+        scale = 0.001
+        x, y = np.random.multivariate_normal(mean, cov, 100).T
         
         xx, yy = np.meshgrid(x,y)
         pos = np.dstack((xx,yy))
         
         rv = multivariate_normal(mean, cov)
-        pdf = rv.pdf(pos)
+        pdf = rv.pdf(pos)/scale
 
         X, Y = np.mgrid[-5:5:0.1, -5:5:0.1]
         pos = np.dstack((X,Y))
         plt.figure()
-        plt.contourf(X, Y, rv.pdf(pos))
+        plt.contourf(X, Y, rv.pdf(pos)/scale)
         plt.colorbar()
         plt.show()
 
 
         posMeas = np.dstack((x,y))
-        posMeas = posMeas.reshape(10,2)
+        posMeas = posMeas.reshape(100,2)
         
         y = pdf[0][:]
-
+        mean = pyGPs.mean.Zero()
+        cov = pyGPs.cov.RBF()
         model = pyGPs.GPR()      # specify model (GP regression)
+        model.setPrior(mean=mean, kernel=cov)
         model.getPosterior(posMeas, y) # fit default model (mean zero & rbf kernel) with data
         model.optimize(posMeas, y)     # optimize hyperparamters (default optimizer: single run minimize)
         
@@ -84,10 +58,7 @@ class GaussianProcess:
         z = z.reshape(10000,2)
 
         model.predict(z)         # predict test cases
-#        model.plot()             # and plot result
-        
-        
-        
+
         plt.figure()
         plt.contourf(X, Y, model.ym.reshape(100,100))
         plt.colorbar()
